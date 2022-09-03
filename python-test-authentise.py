7fa282a19e324b86ec30dbbcafc5ad288a871c23
jonathanplatts <44646971+jonathanplatts@users.noreply.github.com>
2022-09-03 17:33:45 +0100

    Add files via upload

--- python-test-authentise.py
+++ python-test-authentise.py
@@ -1,14 +1,54 @@
-# Task 1: Program to reverse a string
+# Task 2: Program to reverse words in a string
+# Add task 1 to a function for use later
 
-print("Please enter a string: ")
+def ReverseString(userInput):
+    userInput = input()
 
-userInput = input()
+    inputCharList = list(userInput)
+    outputCharList = inputCharList[::-1]
+    outputString = ""
 
-inputCharList = list(userInput)
-outputCharList = inputCharList[::-1]
-outputString = ""
-
-for c in outputCharList:
-    outputString += c
+    for c in outputCharList:
+        outputString += c
     
-print(outputString)
+    print(outputString)
+    
+def main():
+    print("Please enter a sentence: ")
+
+    userInput = input()
+
+    ## Easy way
+    # inputWordList = userInput.split()
+    # reversedWordList = inputWordList[::-1]
+    # outputWordList = ""
+
+    # for w in reversedWordList:
+    #     outputWordList += w
+    #     outputWordList += " "
+
+    # print(outputWordList)
+
+    ## And without String.Split()...
+
+    inputWordList = []
+    lastSpacePosition = -1
+    for count, char in enumerate(userInput):
+        if char == " ":
+            word = userInput[lastSpacePosition + 1: count]
+            inputWordList += [word]
+            lastSpacePosition = count     
+            
+    inputWordList += [userInput[lastSpacePosition + 1:]]
+        
+    reversedWordList = inputWordList[::-1]
+    outputWordsStr = ""
+
+    for w in reversedWordList:
+        outputWordsStr += w
+        outputWordsStr += " "
+
+    print(outputWordsStr)
+    
+if __name__ == "__main__":
+    main()
