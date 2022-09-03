8fccde87b98dcb2dbdc0f14ad256d4d9f39a9229
jonathanplatts <44646971+jonathanplatts@users.noreply.github.com>
2022-09-03 17:40:54 +0100

    Task 3: allow reversal of string or word
    Task 1 and Task 2 now refactored into separate methods
    Parsing of string introduced to distinguish between the user-entered string, and the flag -r or -w
    The flag decides which method to call and the string is passed into the relevant method

--- python-test-authentise.py
+++ python-test-authentise.py
@@ -1,54 +1,88 @@
-# Task 2: Program to reverse words in a string
-# Add task 1 to a function for use later
+# Task 3: Program to reverse words in a string
 
-def ReverseString(userInput):
-    userInput = input()
-
-    inputCharList = list(userInput)
+def ReverseString(inputStr):
+    """Takes an input string and completely reverses it from first character to last"""
+    inputCharList = list(inputStr)
     outputCharList = inputCharList[::-1]
     outputString = ""
 
     for c in outputCharList:
         outputString += c
-    
-    print(outputString)
-    
-def main():
-    print("Please enter a sentence: ")
+        
+    return outputString
 
-    userInput = input()
 
-    ## Easy way
-    # inputWordList = userInput.split()
-    # reversedWordList = inputWordList[::-1]
-    # outputWordList = ""
+def ReverseWordsEasy(inputStr):
+    """Takes an input string and reverses the words"""
+    inputWordList = inputStr.split()
+    reversedWordList = inputWordList[::-1]
+    outputWordList = ""
 
-    # for w in reversedWordList:
-    #     outputWordList += w
-    #     outputWordList += " "
+    for w in reversedWordList:
+        outputWordList += w
+        outputWordList += " "
 
-    # print(outputWordList)
+    return outputWordList
 
-    ## And without String.Split()...
 
+def ReverseWords(inputStr):
+    """Reverses the words in a string without use of String.Split()"""
     inputWordList = []
     lastSpacePosition = -1
-    for count, char in enumerate(userInput):
+    for count, char in enumerate(inputStr):
         if char == " ":
-            word = userInput[lastSpacePosition + 1: count]
+            word = inputStr[lastSpacePosition + 1: count]
             inputWordList += [word]
             lastSpacePosition = count     
             
-    inputWordList += [userInput[lastSpacePosition + 1:]]
-        
+    inputWordList += [inputStr[lastSpacePosition + 1:]]
+    
+    #print("input: ", inputWordList)
     reversedWordList = inputWordList[::-1]
+    #print("reversed: ", reversedWordList)
+    
     outputWordsStr = ""
 
     for w in reversedWordList:
         outputWordsStr += w
         outputWordsStr += " "
 
-    print(outputWordsStr)
+    return outputWordsStr
+
+def main():
+    stringIdxs = []
+    flagIdxs = []
+    flag = ""
     
+    print("Please enter a string in quotes '' or "", with a flag - -r or -w: ")
+    userInput = input()
+    
+    for count, char in enumerate(userInput):
+        if char == "'" or char == '"':
+            stringIdxs += [count]
+        if char == "-":
+            flagIdxs += [(count + 1)]
+
+    stringStartIdx = min(stringIdxs)
+    stringEndIdx = max(stringIdxs)
+
+    userInputStr = userInput[stringStartIdx + 1 : stringEndIdx]
+        
+    flagIdxs = [x for x in flagIdxs if x > stringEndIdx] # Only a '-' outside the input string can be the flag
+
+    if len(flagIdxs) > 0:
+        flag = userInput[max(flagIdxs)]
+    else:
+        raise Exception("No flag specified")
+
+    if flag == "r":
+        resultToPrint = ReverseString(userInputStr)
+    elif flag == "w":
+        resultToPrint = ReverseWords(userInputStr)
+    else:
+        raise Exception("Only flags -r and -w are allowed")
+
+    print(resultToPrint)
+
 if __name__ == "__main__":
     main()
